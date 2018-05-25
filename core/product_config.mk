LineageOS/android_build

build: build generic lineage targets from vendor/lineage
Change-Id: I89f85dfd08aa6bf03faeacedc6a91882121b0df1

core/product_config.mk
@@ -171,6 +171,7 @@ include $(BUILD_SYSTEM)/device.mk
 # A Lineage build needs only the Lineage product makefiles.
 ifneq ($(LINEAGE_BUILD),)
   all_product_configs := $(shell find device -path "*/$(LINEAGE_BUILD)/lineage.mk")
+  all_product_configs += $(wildcard vendor/lineage/build/target/product/lineage_$(LINEAGE_BUILD).mk)
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.

Cardinal-AOSP/build

Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

core/product_config.mk
@@ -171,6 +171,7 @@ include $(BUILD_SYSTEM)/device.mk
 # A Cardinal build needs only the Cardinal product makefiles.
 ifneq ($(CARDINAL_BUILD),)
   all_product_configs := $(shell find device -path "*/$(CARDINAL_BUILD)/cardinal.mk")
+  all_product_configs += $(wildcard vendor/cardinal/build/target/product/cardinal_$(CARDINAL_BUILD).mk)
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.